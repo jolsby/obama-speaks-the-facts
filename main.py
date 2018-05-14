3f1dc8f8aa152baee738f7a8bad5293c29b7e490
jolsby <32502367+jolsby@users.noreply.github.com>
2018-05-13 19:23:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ app = Flask(__name__)
 def get_fact():
 
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+	site = 'http://talkobamato.me/synthesize.py'
+	text = {'input_text': get_fact()}
+
+	r = requests.post(url=site,
+						data = text,
+						allow_redirects = False)
+
+	h = r.headers['Location']
+
+	return h
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
